78a599038cab900ba0312927a87780cf57b3e1ea
Nery Chapetón-Lamas <nerychapeton@gmail.com>
2026-02-26 02:11:51 +0000

    added setters, getters, and full constructor to HealthTracker

--- HealthTracker.java
+++ HealthTracker.java
@@ -1,12 +1,25 @@
+/**
+ * CLASS INVARIANT
+ * - height must be in total inches
+ */
 public class HealthTracker {
     // constants
+    public static final double DEFAULT_WEIGHT = 100;
+    public static final int DEFAULT_HEIGHT = 100;
 
     // instance vars
     private double weight;
-    private double height;
-    private double bmi;
+    private int height; // in inches
 
     // constructors
+    public HealthTracker(double weight, int height) {
+        boolean isValid = this.setWeight(weight) && this.setHeight(height);
+        if (!isValid) {
+            // handle error
+            System.out.println("ERROR: bad data given to full constructor");
+            System.exit(0);
+        }
+    }
 
     // mutators
     public boolean setWeight(double weight) {
@@ -18,16 +31,40 @@ public class HealthTracker {
         }
     }
 
+    public boolean setHeight(int height) {
+        if (height > 0) { // only set if valid data
+            this.height = height;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean setHeight(int feet, int inches) {
+        int totalInches = feet * 12 + inches;
+        return setHeight(totalInches);
+    }
+
     // accessors
     public double getWeight() {
         return this.weight;
     }
 
+    public double getHeightInInches() {
+        return this.height;
+    }
+
+    public String getHeight() {
+        int feet = this.height / 12;
+        int inches = this.height % 12;
+        return feet + " ft " + inches + " in";
+    }
+
+
     // other required methods
     @Override
     public String toString() {
         return "HealthTracker: weight = " + this.weight +
-                ", height = " + this.height +
-                ", BMI = " + this.bmi;
+                ", height = " + this.height;
     }
 }
--- Main.java
+++ Main.java
@@ -1,21 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        HealthTracker tester = new HealthTracker();
+        HealthTracker tester = new HealthTracker(50, 50);
 
-        System.out.println(tester);
+        System.out.println("Start = " + tester);
 
+        tester.setWeight(205.6);
+        tester.setHeight(100);
+        System.out.println("Height = " + tester.getHeight());
+
+        System.out.println("After changes = " + tester);
         // Test for setWeight method
-        testSetWeight();
+        // testSetWeight();
     }
 
-    public static void testSetWeight() {
-        HealthTracker ht = new HealthTracker();
-        double testWeight = 205.6;
-        boolean isValid = ht.setWeight(testWeight);
-        if (isValid) {
-            System.out.println("setWeight test passed: weight set to " + testWeight);
-        } else {
-            System.out.println("setWeight test failed: expected " + testWeight + ", got " + ht.getWeight());
-        }
-    }
+    // public static void testSetWeight() {
+    // HealthTracker ht = new HealthTracker();
+    // double testWeight = 205.6;
+    // boolean isValid = ht.setWeight(testWeight);
+    // if (isValid) {
+    // System.out.println("setWeight test passed: weight set to " + testWeight);
+    // } else {
+    // System.out.println("setWeight test failed: expected " + testWeight + ", got "
+    // + ht.getWeight());
+    // }
+    // }
 }
