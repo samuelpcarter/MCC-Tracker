7bb1dc590d30c45560189bacf1f0ea11bc9d55bc
Nery Chapetón-Lamas <nerychapeton@gmail.com>
2026-04-08 20:46:48 -0700

    trialed tabpane and started wireframe
    tried out tab pane to see how it works, and started wireframe for home tab but all work is rough and experimental

--- src/main/java/edu/miracosta/cs112/controllers/MainController.java
+++ src/main/java/edu/miracosta/cs112/controllers/MainController.java
@@ -2,11 +2,16 @@ package edu.miracosta.cs112.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 
 public class MainController {
+
     @FXML
-    protected void onHelloButtonClick(ActionEvent actionEvent) {
+    Button hiButton;
+
+    @FXML
+    protected void onHelloButtonClicked(ActionEvent actionEvent) {
         System.out.println("Hello World!");
     }
 }
